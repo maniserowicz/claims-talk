6f0f4e28b2952672a60887cf4e61b112d93a88a3
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2013-02-08 07:00:04 +0100

    added Windows identity/principal demos

---
+++ Claims.WG.NET/Claims.WG.NET/Windows_demos.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Principal;
+using Xunit;
+
+public class Windows_demos
+{
+    private dynamic get_current_user()
+    {
+        var id = WindowsIdentity.GetCurrent();
+        var principal = new WindowsPrincipal(id);
+
+        return new
+        {
+            id,
+            principal
+        };
+    }
+
+    [Fact]
+    public void current_windows_user()
+    {
+        var user = get_current_user();
+
+        Assert.Equal("p-win8\\procent", user.id.Name);
+    }
+
+    [Fact]
+    public void is_user_string()
+    {
+        var user = get_current_user();
+
+        // culture-specific
+        bool isUser = user.principal.IsInRole("Builtin\\Users");
+
+        Assert.True(isUser);
+    }
+
+    [Fact]
+    public void is_admin_sid()
+    {
+        var user = get_current_user();
+
+        var builtinUsers = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
+
+        bool isUser = user.principal.IsInRole(builtinUsers);
+
+        Assert.True(isUser);
+    }
+}
